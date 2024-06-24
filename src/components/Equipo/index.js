cc1c34d0efcd8ed5ff0a568c097eecb7311161f3
legive <leylagisela@gmail.com>
2024-06-24 16:36:51 -0600

    Creando funciones

--- src/components/Equipo/index.js
+++ src/components/Equipo/index.js
@@ -1,26 +1,42 @@
-import "./Equipo.css"
-import Colaborador from "../Colaborador"
+import "./Equipo.css";
+import Colaborador from "../Colaborador";
+import hexToRgba from 'hex-to-rgba';
 
 const Equipo = (props) => {
-    //Destructuracion
-    const { titulo, colorPrimario, colorSecundario } = props.datos
-    const { colaboradores, eliminarColaborador, actualizarColor } = props
-    
-    
-    
-    const estiloTitulo = { borderColor: colorPrimario }
-    console.log(colaboradores.length)
-    
-    return colaboradores.length >0?
-        < section className="equipo" style={{ backgroundColor: colorSecundario }
-        }>
-            <input type="color" value={colorSecundario} className="input-color" onChange={(evento)=>actualizarColor(evento.target.value,titulo)}></input>
-        <h3 style={estiloTitulo}>{titulo}</h3>
-        <div className="colaboradores">
-            {colaboradores.map((colaborador, index) => <Colaborador eliminarColaborador={eliminarColaborador} key={index} colaborador={colaborador} color={colorPrimario} />)}
-        </div>
-    </section >: <div></div>
-    
-}
+  //Destructuracion
+  const { titulo, colorPrimario, colorSecundario } = props.datos;
+  const { colaboradores, eliminarColaborador, actualizarColor } = props;
 
-export default Equipo
+  const obj = {
+      backgroundColor: hexToRgba(colorPrimario, '0.2'),
+  };
+
+  const estiloTitulo = { borderColor: colorPrimario };
+  console.log(colaboradores.length);
+
+  return colaboradores.length > 0 ? (
+      <section className="equipo" style={obj }>
+      <input
+        type="color"
+        value={colorPrimario}
+        className="input-color"
+        onChange={(evento) => actualizarColor(evento.target.value, titulo)}
+      ></input>
+      <h3 style={estiloTitulo}>{titulo}</h3>
+      <div className="colaboradores">
+        {colaboradores.map((colaborador, index) => (
+          <Colaborador
+            eliminarColaborador={eliminarColaborador}
+            key={index}
+            colaborador={colaborador}
+            color={colorPrimario}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    <div></div>
+  );
+};
+
+export default Equipo;
